cd9d4d3411641a47d13f5fa708f5b392b92a8b9e
balandinS <35811817+balandinS@users.noreply.github.com>
2018-10-22 23:28:33 +0300

    upgrade home and detail watch
    upgrade home and detail watch

--- src/app/watches/watch-detiel/watch-detiel.component.ts
+++ src/app/watches/watch-detiel/watch-detiel.component.ts
@@ -1,29 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { WatchModel } from '../watche.model';
 import { WatchesService } from '../watches.service';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-watch-detiel',
   templateUrl: './watch-detiel.component.html',
   styleUrls: ['./watch-detiel.component.scss']
 })
-export class WatchDetielComponent implements OnInit {
+export class WatchDetielComponent implements OnInit, OnDestroy {
   watch: WatchModel;
   index: number;
+  subacription = new Subscription();
+  @ViewChild('title', {read: ElementRef} ) title : ElementRef;
   constructor(
     private watchService: WatchesService,
     private route: ActivatedRoute
     ) { }
    
   ngOnInit() {
-    this.route.params.subscribe(params => {
+   this.subacription = this.route.params.subscribe(params => {
       this.index = +params['id'];
       this.watch = this.watchService.getWatch(this.index);
       console.log(this.watch.img.src)
     })
     
   }
-    
 
+
+  show(img : any) {
+    this.title.nativeElement.src = img.src;
+  }
+    
+ ngOnDestroy() {
+   this.subacription.unsubscribe;
+ }
 }
